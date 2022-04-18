8e8dba7cadaea395e10ac6cfcd6b6d82c5724b46
admin <admin@DESKTOP-8CLJC3O>
2022-04-18 09:37:44 +0900

    22.04.18. java test

---
+++ p20220415/src/co/yedam/war/Exaple.java
@@ -0,0 +1,19 @@
+package co.yedam.war;
+
+public class Exaple {
+
+	public static void main(String[] args) {
+		//반지름이 5인 원의 면적.
+		
+		double PI = 3.14;
+		int radius = 5;
+		System.out.println(PI*radius*radius);
+		
+		//밑면이 4, 높이는 4인 삼각형의 넓이를 구하시오.
+		int r = 4;
+		int h = 4;
+		double tArea = r * h / 2;
+		System.out.println(tArea);
+	}
+
+}
