55fccda3ee5b9e16018dcf282d406a298f2122e3
admin <admin@YD4-02>
2022-04-20 15:54:50 +0900

    20220420 Array, field, class

---
+++ P20220420/src/co/edu/array/ArrayExam5.java
@@ -0,0 +1,17 @@
+package co.edu.array;
+
+public class ArrayExam5 {
+
+	public static void main(String[] args) {
+		
+		//참조타입 (String) 배열 생성
+		String[] strAry = {"김","이", "박", "최"};
+		
+		for(String firName : strAry) {		//참조타입과 출력 타입(?)이 같아야한다.
+			System.out.println(firName);
+		}
+		
+		
+	}
+
+}
